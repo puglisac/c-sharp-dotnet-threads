7ff17803d41f0f178fab8e55acfc3fb6bfa967da
puglisac <alan.c.puglisi@gmail.com>
2020-11-18 14:34:32 -0600

    finished section 5

---
+++ LearningThreads/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LearningThreads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Thread myThread = new Thread(new ThreadStart(Work));
+                myThread.Start();
+
+                Task.Run(() =>
+                {
+                    Console.WriteLine("starting task in thread: " + Thread.CurrentThread.ManagedThreadId);
+                    Thread.Sleep(3000);
+                    Console.WriteLine("Task complete");
+                });
+            }
+
+        }
+        static void Work()
+        {
+            Console.WriteLine("starting in thread: " + Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(3000);
+            Console.WriteLine("Work complete");
+        }
+    }
+}
